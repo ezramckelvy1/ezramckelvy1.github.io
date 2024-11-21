ad4c873a4e346a8e503a51bdaf22fadf15893ddb
EzraMckelvy <143837407+ezramckelvy1@users.noreply.github.com>
2024-11-21 15:31:37 +0000

    saving walker

--- asd-walker-master/index.js
+++ asd-walker-master/index.js
@@ -13,16 +13,30 @@ function runProgram() {
     RIGHT: 39,
     UP: 38,
     DOWN: 40,
+    W: 87,
+    A: 65,
+    S: 83,
+    D: 68,
   }
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
 
   // Game Item Objects
   var walker = {
+    PosX: 350,
+    SpeedX: 0,
+    PosY: 350,
+    SpeedY: 0,
+    tagged: true,
+    color: "Red",
+  }
+  var walkerTwo = {
     PosX: 0,
     SpeedX: 0,
     PosY: 0,
     SpeedY: 0,
+    tagged: false,
+    color: "Teal",
   }
 
   // one-time setup
@@ -38,7 +52,9 @@ function runProgram() {
   by calling this function and executing the code inside.
   */
   function newFrame() {
+    walkerColor()
     repositionGameItem()
+    walkerCollision()
     wallCollision()
     redrawGameItem()
   }
@@ -46,49 +62,157 @@ function runProgram() {
   /* 
   Called in response to events.
   */
+
+
   function handleKeyUp(event) {
-    walker.SpeedX = 0
-    walker.SpeedY = 0
-  }
-  function handleKeyDown(event) {
+    
+    //first walker//
     if (event.which === KEY.LEFT) {
-      walker.SpeedX = -5
+      walker.SpeedX = 0
+
     }
-    if (event.which === KEY.RIGHT) {
-      walker.SpeedX = 5
+    if (event.which === KEY.RIGHT) {  
+      walker.SpeedX = 0
     }
     if (event.which === KEY.UP) {
-      walker.SpeedY = -5
+      walker.SpeedY = 0
     }
     if (event.which === KEY.DOWN) {
-      walker.SpeedY = 5
+      walker.SpeedY = 0
     }
+    //second walker//
+    if (event.which === KEY.A) {
+      walkerTwo.SpeedX = 0
+    }
+    if (event.which === KEY.D) {
+      walkerTwo.SpeedX = 0
+    }
+    if (event.which === KEY.W) {
+      walkerTwo.SpeedY = 0
+    }
+    if (event.which === KEY.S) {
+      walkerTwo.SpeedY = 0
+    }
+
+  }
+  
+  function handleKeyDown(event) {
+
+    //first walker//
+    if (event.which === KEY.LEFT) {
+      walker.SpeedX = walker.tagged ? -3 : -5
+
+    }
+    if (event.which === KEY.RIGHT) {
+      walker.SpeedX = walker.tagged ? 3 : 5
+    }
+    if (event.which === KEY.UP) {
+      walker.SpeedY = walker.tagged ? -3 : -5
+    }
+    if (event.which === KEY.DOWN) {
+      walker.SpeedY = walker.tagged ? 3 : 5
+    }
+    //second walker//
+    if (event.which === KEY.A) {
+      walkerTwo.SpeedX = walkerTwo.tagged ? -3 : -5
+    }
+    if (event.which === KEY.D) {
+      walkerTwo.SpeedX = walkerTwo.tagged ? 3 : 5
+    }
+    if (event.which === KEY.W) {
+      walkerTwo.SpeedY = walkerTwo.tagged ? -3 : -5
+    }
+    if (event.which === KEY.S) {
+      walkerTwo.SpeedY = walkerTwo.tagged ? 3 : 5
+    }
+
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+  function walkerCollision() {
+    if (walker.PosX + 45 >= walkerTwo.PosX &&
+      walker.PosX <= walkerTwo.PosX + 45 &&
+      walker.PosY + 45 >= walkerTwo.PosY &&
+      walker.PosY <= walkerTwo.PosY + 45) {
+      tagged()
+      colorChange()
+
+
+    }
+  }
+
+  function colorChange() {
+    if (walker.color === "Red") {
+      walker.color = "Teal"
+      walkerTwo.color = "Red"
+    } else {
+      walkerTwo.color = "Teal"
+      walker.color = "Red"
+    }
+  }
+  function walkerColor() {
+    $("#walker").css("background-color", walker.color)
+    $("#walkerTwo").css("background-color", walkerTwo.color)
+  }
+  function tagged() {
+    if (walker.tagged) {
+      walker.tagged = false
+      walkerTwo.tagged = true
+
+      posReset()
+    } else {
+      walker.tagged = true
+      walkerTwo.tagged = false
+      posReset()
+    }
+  }
+  function posReset() {
+    walker.PosX = 350
+    walker.PosY = 350
+    walkerTwo.PosX = 0
+    walkerTwo.PosY = 0
+  }
   function wallCollision() {
+    //walker
     if (walker.PosX < 0) {
       walker.PosX -= walker.SpeedX
     }
     if (walker.PosY < 0) {
       walker.PosY -= walker.SpeedY
     }
-    if (walker.PosX >= $("#board").width()-45) {
+    if (walker.PosX >= $("#board").width() - 45) {
       walker.PosX -= walker.SpeedX
     }
-    if (walker.PosY >= $("#board").height()-45) {
+    if (walker.PosY >= $("#board").height() - 45) {
       walker.PosY -= walker.SpeedY
+    }
+    //walker two
+    if (walkerTwo.PosX < 0) {
+      walkerTwo.PosX -= walkerTwo.SpeedX
+    }
+    if (walkerTwo.PosY < 0) {
+      walkerTwo.PosY -= walkerTwo.SpeedY
+    }
+    if (walkerTwo.PosX >= $("#board").width() - 45) {
+      walkerTwo.PosX -= walkerTwo.SpeedX
+    }
+    if (walkerTwo.PosY >= $("#board").height() - 45) {
+      walkerTwo.PosY -= walkerTwo.SpeedY
     }
   }
   function repositionGameItem() {
     walker.PosX += walker.SpeedX
-    walker.PosY = walker.PosY + walker.SpeedY
+    walker.PosY += walker.SpeedY
+    walkerTwo.PosX += walkerTwo.SpeedX
+    walkerTwo.PosY += walkerTwo.SpeedY
   }
   function redrawGameItem() {
     $("#walker").css("left", walker.PosX)
     $("#walker").css("top", walker.PosY)
+    $("#walkerTwo").css("left", walkerTwo.PosX)
+    $("#walkerTwo").css("top", walkerTwo.PosY)
   }
   function endGame() {
     // stop the interval timer
